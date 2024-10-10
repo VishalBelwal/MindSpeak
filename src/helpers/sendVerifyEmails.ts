81a7baf1f9095e99f49b9718288dc8541f5e6983
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-10 16:57:22 +0530

    sending and accepting emails

---
+++ src/helpers/sendVerifyEmails.ts
@@ -0,0 +1,24 @@
+import {resend} from "@/lib/resend"
+import VerificationEmail from "../../emails/verifyEMAILS"
+import { ApiResponse } from "@/types/ApiResponse"
+
+export async function sendVerifyEmail(
+    email: string,
+    userName: string,
+    otp: string
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: "MindSpeak Verification Email",
+            react: VerificationEmail({username: userName, otp: otp})
+        })
+
+        return {success: true, message: "Verification email sent successfully"}
+        
+    } catch (emailError) {
+        console.error("error sending verification email", emailError)
+        return {success: false, message: "Failed to send verification email"}
+    }
+}
