a3bba22f4359ba842c685032cf7622ba39c39055
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-11 22:38:20 +0530

    responses

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,9 @@
+import { Message } from "@/model/User.model";
+
+export interface ApiResponse{
+    success: boolean;
+    message: string;
+    isAcceptingMessages?: boolean;
+    messages?: Array<Message>
+
+}
