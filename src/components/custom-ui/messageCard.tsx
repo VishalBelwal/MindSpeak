e7e8ab9e0aa110b87d1d58a180f151cfa626b003
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-26 23:48:05 +0530

    components modified

--- src/components/custom-ui/messageCard.tsx
+++ src/components/custom-ui/messageCard.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -26,6 +19,7 @@ import { Message } from "@/model/User.model";
 import { useToast } from "@/hooks/use-toast";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
+import dayjs from 'dayjs';
 
 type MessageCardProp = {
   message: Message;
@@ -43,36 +37,34 @@ const MessageCard = ({ message, onMessageDelete }: MessageCardProp) => {
         title: response.data.message,
       });
       onMessageDelete(message._id);
-
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
-        title: 'Error',
+        title: "Error",
         description:
-          axiosError.response?.data.message ?? 'Failed to delete message',
-        variant: 'destructive',
+          axiosError.response?.data.message ?? "Failed to delete message",
+        variant: "destructive",
       });
-    } 
+    }
   };
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
+        <CardTitle>{message.content}</CardTitle>
 
         <AlertDialog>
           <AlertDialogTrigger asChild>
             <Button variant="destructive">
-              {" "}
-              <X className="w-5 h-5" />{" "}
+              <X className="w-5 h-5" />
             </Button>
           </AlertDialogTrigger>
           <AlertDialogContent>
             <AlertDialogHeader>
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
+                This action cannot be undone. This will permanently delete this
+                message.
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
@@ -84,11 +76,10 @@ const MessageCard = ({ message, onMessageDelete }: MessageCardProp) => {
           </AlertDialogContent>
         </AlertDialog>
 
-        <CardDescription>Card Description</CardDescription>
+        <div className="text-sm">
+          { dayjs(message.createdAt).format('MMM D, YYYY h:mm A') }
+        </div>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
     </Card>
   );
 };
--- src/components/custom-ui/navbar.tsx
+++ src/components/custom-ui/navbar.tsx
@@ -13,9 +13,13 @@ const Navbar = () => {
 
   const word = [
     {
-      text: "MindSpeak"
-    }
-  ]
+      text: "MindSpeak",
+    },
+  ];
+
+  const handleSignOut = () => {
+    signOut({ callbackUrl: "/" });
+  };
 
   return (
     <nav className="p-4 md:p-5 text-white bg-[#242424]">
@@ -28,13 +32,22 @@ const Navbar = () => {
             <span className="mr-4 tracking-tight font-semibold text-lg">
               Welcome, {user.userName?.toLocaleUpperCase() || user.email}
             </span>
-            <Button onClick={() => signOut()} className="w-full md:w-auto p-4 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 hover:text-slate-300 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 z-20" variant='outline'>
+            <Button
+              onClick={handleSignOut}
+              className="w-full md:w-auto p-4 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 hover:text-slate-300 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 z-20"
+              variant="outline"
+            >
               Logout
             </Button>
           </>
         ) : (
           <Link href="/sign-in">
-            <Button className="w-full md:w-auto p-4 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 hover:text-slate-300 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" variant='outline'>Login</Button>
+            <Button
+              className="w-full md:w-auto p-4 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 hover:text-slate-300 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              variant="outline"
+            >
+              Login
+            </Button>
           </Link>
         )}
       </div>
