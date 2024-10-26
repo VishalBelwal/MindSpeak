0356c9b62feb6626760cf312366850bd355de696
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-26 23:49:06 +0530

    dashboar modify

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useToast } from "@/hooks/use-toast";
 import { Message } from "@/model/User.model";
 import { acceptMessagesSchema } from "@/schemas/acceptMessageSchema";
@@ -23,12 +22,9 @@ function Dashboard() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSwitchLoading, setIsSwitchLoading] = useState(false);
-
   const { toast } = useToast();
 
   const handleDeleteMessage = (messageId: string) => {
-    //checking if message id in messages exist or not
-    //basically example of optimistic ui i.e. first you update then api calls will take place with backend
     setMessages(messages.filter((message) => message._id !== messageId));
   };
 
@@ -37,14 +33,11 @@ function Dashboard() {
     resolver: zodResolver(acceptMessagesSchema),
   });
 
-
   const { register, watch, setValue } = form;
-
-  //injecting watch(mandotary)
   const acceptMessages = watch("acceptMessages");
+
   const fetchAcceptMessage = useCallback(async () => {
     setIsSwitchLoading(true);
-
     try {
       const response = await axios.get<ApiResponse>("/api/accept-message");
       setValue("acceptMessages", response.data.isAcceptingMessages);
@@ -52,9 +45,7 @@ function Dashboard() {
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
         title: "Error",
-        description:
-          axiosError.response?.data.message ||
-          "Failed to fetch message settings",
+        description: axiosError.response?.data.message || "Failed to fetch message settings",
         variant: "destructive",
       });
     } finally {
@@ -62,47 +53,33 @@ function Dashboard() {
     }
   }, [setValue, toast]);
 
-  const fetchAllMessages = useCallback(
-    async (refresh: boolean = false) => {
-      setIsLoading(true);
-      setIsSwitchLoading(true);
-
-      try {
-        const response = await axios.get<ApiResponse>("/api/get-message");
-        setMessages(response.data.messages || []);
-        if (refresh) {
-          toast({
-            title: "Refreshed Messages",
-            description: "Showing Latest Messages",
-          });
-        }
-      } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-        //have to ommentall this code because it is disturbing the frontend flow
-        // const message = axiosError.response?.data?.message ?? "Failed to fetch message ";
-        // toast({
-        //   title: "Error",
-        //   description: message,
-        //   variant: "destructive",
-        // });
-
-      } finally {
-        setIsLoading(false);
-        setIsSwitchLoading(false);
+  const fetchAllMessages = useCallback(async (refresh: boolean = false) => {
+    setIsLoading(true);
+    setIsSwitchLoading(true);
+    try {
+      const response = await axios.get<ApiResponse>("/api/get-message");
+      setMessages(response.data.messages || []);
+      if (refresh) {
+        toast({
+          title: "Refreshed Messages",
+          description: "Showing Latest Messages",
+        });
       }
-    },
-    [setIsLoading, setMessages, toast]
-  );
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      // Handle the error appropriately here
+    } finally {
+      setIsLoading(false);
+      setIsSwitchLoading(false);
+    }
+  }, [setIsLoading, setMessages, toast]);
 
   useEffect(() => {
     if (!session || !session.user) return;
     fetchAcceptMessage();
-
-    //get to know the state of messages
     fetchAllMessages();
-  }, [session, setValue, fetchAcceptMessage, fetchAllMessages, toast]);
+  }, [session, setValue, fetchAcceptMessage, fetchAllMessages]);
 
-  //handle switch changes functionality
   const handleSwitchChange = async () => {
     try {
       const response = await axios.post<ApiResponse>("/api/accept-message", {
@@ -117,9 +94,7 @@ function Dashboard() {
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
         title: "Error",
-        description:
-          axiosError.response?.data.message ||
-          "Failed to fetch message settings",
+        description: axiosError.response?.data.message || "Failed to fetch message settings",
         variant: "destructive",
       });
     }
@@ -130,31 +105,26 @@ function Dashboard() {
   }
 
   const { userName } = session?.user as User;
-  //constructing hostURL
   const hostURL = `${window.location.protocol}//${window.location.host}`;
   const profileURL = `${hostURL}/u/${userName}`;
 
   const copyToClipboard = () => {
-    //can be easily accessible because we are in client component we can get it easily can't get these in server component
     navigator.clipboard.writeText(profileURL);
     toast({
       title: "URL Copied Successfully",
-      description: "Profile Url copied Successfully",
+      description: "Profile URL copied successfully",
     });
   };
 
   return (
-    <div>
+    <div className="relative min-h-screen text-white bg-neutral-900 flex flex-col">
       <Navbar />
-      <div className="h-[40rem] text-white bg-neutral-900 flex flex-col items-center justify-center relative w-full mx-8 md:mx-8 lg:mx-auto">
-        <div className="z-20">
-          <h1 className="text-white text-4xl font-bold mb-4">User Dashboard</h1>
-
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <div className="w-full max-w-3xl p-4 z-20">
+          <h1 className="text-4xl font-bold mb-4 text-center tracking-tight">User Dashboard</h1>
           <div className="mb-6">
-            <h2 className="text-lg font-semibold mb-4 text-white">
-              Copy Your Unique Link
-            </h2>{" "}
-            <div className="flex items-center z-10">
+            <h2 className="text-lg font-semibold mb-4 text-center">Copy Your Unique Link</h2>
+            <div className="flex items-center">
               <input
                 type="text"
                 value={profileURL}
@@ -163,26 +133,24 @@ function Dashboard() {
               />
               <Button
                 onClick={copyToClipboard}
-                className="bg-white hover:bg-white text-black"
+                className="bg-white text-black hover:bg-white"
               >
                 Copy
               </Button>
             </div>
-            <div className="mb-4">
+            <div className="mt-4 flex items-center justify-center">
               <Switch
                 {...register("acceptMessages")}
                 checked={acceptMessages}
                 onCheckedChange={handleSwitchChange}
                 disabled={isSwitchLoading}
-                className="text-black outline  outline-white mt-4 mr-2"
+                className="text-black outline outline-white mr-2"
               />
-              <span className="ml-2">
-                Accept Messages: {acceptMessages ? "On" : "Off"}
-              </span>
+              <span className="font-bold">Accept Messages: {acceptMessages ? "On" : "Off"}</span>
             </div>
-            <Separator />
+            <Separator className="my-4" />
             <Button
-              className="mt-4 z-10"
+              className="flex items-center justify-start"
               variant="outline"
               onClick={(e) => {
                 e.preventDefault();
@@ -195,24 +163,24 @@ function Dashboard() {
                 <RefreshCcw className="h-4 text-black w-4" />
               )}
             </Button>
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
               {messages.length > 0 ? (
-                messages.map((message, index) => (
+                messages.map((message) => (
                   <MessageCard
-                    key={message._id ? message._id.toString() : index}
+                    key={String(message._id)}
                     message={message}
                     onMessageDelete={handleDeleteMessage}
                   />
                 ))
               ) : (
-                <p>No messages to display.</p>
+                <p className="text-center font-semibold tracking-normal text-lg">No Messages to Display.</p>
               )}
             </div>
           </div>
         </div>
       </div>
-      <ShootingStars />
-      <StarsBackground />
+      <ShootingStars className="absolute inset-0 z-0" />
+      <StarsBackground className="absolute inset-0 z-0" />
     </div>
   );
 }
